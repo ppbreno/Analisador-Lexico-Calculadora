89b5dd0ec40280e322abaf5e8d669141cd515227
ppbreno <102385983+ppbreno@users.noreply.github.com>
2025-02-26 12:36:16 -0300

    criação da tela inicial

---
+++ src/analisadorlexico/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package analisadorlexico;
+
+import UI.TelaInicial;
+
+/**
+ *
+ * @author T-Gamer
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaInicial inicio = new TelaInicial();
+        inicio.setVisible(true);
+    }
+    
+}
